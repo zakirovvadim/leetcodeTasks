730918a07c194be09d9b85f1d474476fd3333d86
Вадим Закиров <vadimzakirov@MacBook-Pro-Vadim.local>
2023-04-30 22:54:39 +0500

    1 commit

---
+++ src.java.vadim.ru.leetcode/longestPrefix/Prefix.java
@@ -0,0 +1,12 @@
+package longestPrefix;
+
+public class Prefix {
+    public static void main(String[] args) {
+        String [] str = {"flower","flow","flight"};
+        char symbol = 0;
+        for (int i = 0; i < str.length; i++) {
+            char[] symbols = str[i].toCharArray();
+            char symbol1 = symbols[i];
+        }
+    }
+}
