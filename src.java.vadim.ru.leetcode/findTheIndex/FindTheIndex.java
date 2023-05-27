b62ca92ac54887bac12af8326c8d250ff0006409
VadimZakirov <appolon17@gmail.com>
2023-05-27 17:27:27 +0500

    resolve 28.Find the Index of the First Occurrence in a...

---
+++ src.java.vadim.ru.leetcode/findTheIndex/FindTheIndex.java
@@ -0,0 +1,36 @@
+package findTheIndex;
+
+import java.nio.charset.StandardCharsets;
+
+public class FindTheIndex {
+    public static void main(String[] args) {
+        int i = strStr("leetcode", "leeto");
+        System.out.println(i);
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (haystack.equals(needle)) {
+            return 0;
+        }
+        char[] bytesNeedle = needle.toCharArray();
+        int lengthNeedle = needle.toCharArray().length;
+        char[] bytesHaysTack = haystack.toCharArray();
+        int lastLand = (haystack.length()) - bytesNeedle.length;
+        for (int i = 0; i < bytesHaysTack.length; i++) {
+            int j = i;
+            if (lengthNeedle > bytesHaysTack.length - i) {
+                return -1;
+            }
+            StringBuilder b = new StringBuilder();
+            for (int k = 0; k < lengthNeedle; k++) {
+                b.append(bytesHaysTack[i]);
+                i++;
+            }
+            i = j;
+            if (b.toString().equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src.java.vadim.ru.leetcode/removeDuplicates/RemoveDuplicates.java
@@ -0,0 +1,7 @@
+package removeDuplicates;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+    }
+}
