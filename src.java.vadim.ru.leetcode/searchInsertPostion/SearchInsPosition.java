9e70db6bc663966e5efdd8ad95a2f281fc69801c
vadimzakirov <vadimzakiroff@gmail.com>
2023-06-04 19:06:11 +0500

    resolve 35 task

--- src.java.vadim.ru.leetcode/searchInsertPostion/SearchInsPosition.java
+++ src.java.vadim.ru.leetcode/searchInsertPostion/SearchInsPosition.java
@@ -13,38 +13,14 @@ public class SearchInsPosition {
     }
 
     public static int searchInsert(int[] nums, int target) {
-        boolean flag = true;
-        int number;
-        List<int[]> firstAndSecondPart = getFirstAndSecondPart(nums);
-        if (target >= firstAndSecondPart.get(0)[firstAndSecondPart.get(0).length - 1]) {
-            if (firstAndSecondPart.get(0).length == 1) {
-                number = Arrays
-                return firstAndSecondPart.get(1)[0];
-            } else {
-                searchInsert(firstAndSecondPart.get(1), target);
-            }
-        } else {
-            if (firstAndSecondPart.get(1).length == 1) {
-                return firstAndSecondPart.get(1)[0];
-            } else {
-                searchInsert(firstAndSecondPart.get(0), target);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            } else if (nums[i] > target) {
+                return i - 1;
             }
         }
+        return -1;
     }
 
-    private static List<int[]> getFirstAndSecondPart(int[] nums) {
-        List<int[]> list = new ArrayList<>();
-        list.add(getFirstPart(nums));
-        list.add(getSecondPart(nums));
-        return list;
-    }
-
-    private static int[] getFirstPart(int[] nums) {
-        int i = (nums.length + 1) / 2;
-        return Arrays.copyOfRange(nums, 0, (nums.length + 1) / 2);
-    }
-
-    private static int[] getSecondPart(int[] nums) {
-        return Arrays.copyOfRange(nums, (nums.length + 1) / 2, nums.length);
-    }
 }
