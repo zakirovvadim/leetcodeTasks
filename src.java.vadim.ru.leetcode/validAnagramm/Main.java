415ebff3a73ee8a608c5a2cbb77727d0f542dea5
Vadim <vadim.zakiroff@gmail.com>
2024-01-02 22:30:55 +0500

    242. Valid Anagram - resolved.

---
+++ src.java.vadim.ru.leetcode/validAnagramm/Main.java
@@ -0,0 +1,21 @@
+package validAnagramm;
+
+import java.util.Arrays;
+
+/**
+ * Просто достаем из строк массива чар и сортируем, потом снова конвертируем в стрингу и сравниваем между собой.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "anagram";
+        String s2 = "nagaram";
+        System.out.println(isAnagram(s1, s2));
+    }
+    public static boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        Arrays.sort(s1);
+        char[] t1 = t.toCharArray();
+        Arrays.sort(t1);
+        return String.valueOf(t1).equals(String.valueOf(s1));
+    }
+}
